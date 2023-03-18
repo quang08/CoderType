5bebbe14375e99e099fcf3b6fec11bac31ed42a3
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-03-18 20:36:31 +0700

    first commit

---
+++ public/scripts/data/index.js
@@ -0,0 +1,9 @@
+import { linuxRepo } from "./linux";
+import { reactRepo } from "./react";
+import { tensorFlowRepo } from "./tensorflow";
+
+export const repoOptions = [
+    linuxRepo,
+    reactRepo,
+    tensorFlowRepo
+]
